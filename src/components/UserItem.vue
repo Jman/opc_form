e5769e4a18a7c76344232fdfad6b2b9b1dfccfce
Eugene Tereshchenko <i@jman.info>
2022-12-04 18:54:02 +0100

    fix: update screenshots, styles refactoring

--- src/components/UserItem.vue
+++ src/components/UserItem.vue
@@ -1,14 +1,14 @@
 <template>
   <tr @click.prevent="$emit('editUser')">
-    <td class="index">
+    <td :class="$style.index">
       {{ index }}
     </td>
-    <td class="name">
+    <td :class="$style.name">
       {{ form.email }}
     </td>
-    <td class="actions">
-      <a href="#">edit</a> |
-      <a href="#" @click.stop.prevent="$emit('deleteUser')">delete</a>
+    <td :class="$style.actions">
+      <button>Edit</button>
+      <button @click.stop.prevent="$emit('deleteUser')">Delete</button>
     </td>
   </tr>
 </template>
@@ -28,3 +28,19 @@ export default {
   emits: ['editUser', 'deleteUser'],
 };
 </script>
+
+<style lang="scss" module>
+.index {
+  width: 1px;
+  padding-right: 0;
+}
+
+.name {
+  text-align: left;
+}
+
+.actions {
+  text-align: right;
+  white-space: nowrap;
+}
+</style>
--- src/options.js
+++ src/options.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import './scss/main.scss';
-import Options from './components/OptionsComponent.vue';
+import Options from './components/OptionsPage.vue';
 import migrateUsers from './common/migrateUsers';
 
 createApp(Options).mount('#options');
